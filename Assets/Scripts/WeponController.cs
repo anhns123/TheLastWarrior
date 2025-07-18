8155a45ca59ef4a9dc2a752824f4d3cd93fd0ed5
DatPM <datpmhe176115@fpt.edu.vn>
2025-07-18 11:20:29 +0700

    dat update

---
+++ Assets/Scripts/WeponController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    [Header("Skill Prefabs")]
+    [SerializeField] private GameObject normalHitPrefab;
+    [SerializeField] private float normalHitCooldown = 0.5f;
+
+    [SerializeField] private GameObject ultiHitPrefab;
+    [SerializeField] private float ultiHitCooldown = 1.5f;
+
+    private Player owner;
+    private bool canAttack = true;
+    private void Awake()
+    {
+        owner = GetComponentInParent<Player>();
+    }
+    private void Update()
+    {
+        if (owner.transform.localScale.x == -1)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+    public void NormalAttack()
+    {
+        if (!canAttack || normalHitPrefab == null) return;
+        SpawnHit(normalHitPrefab, normalHitCooldown);
+    }
+
+    public void UltimateAttack()
+    {
+        if (!canAttack || ultiHitPrefab == null) return;
+        SpawnHit(ultiHitPrefab, ultiHitCooldown);
+    }
+
+    void SpawnHit(GameObject prefab, float duration)
+    {
+        GameObject hitObj = Instantiate(prefab, transform.position, transform.rotation);
+        HitColliderBase hit = hitObj.GetComponent<HitColliderBase>();
+        if (hit != null)
+        {
+            hit.Init(owner, duration);
+        }
+
+        canAttack = false;
+        Invoke(nameof(ResetAttack), duration);
+    }
+    void ResetAttack()
+    {
+        canAttack = true;
+    }
+}
+
+
